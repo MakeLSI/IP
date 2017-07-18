38cf76ec13111e3f25045d191b001f27dbd4e2e2
Junichi Akita <akita@is.t.kanazawa-u.ac.jp>
2017-07-18 20:50:27 +0900

    Glade用DRCルールの修正
    NSL/PSL間隔、POL/VIA重なりなどのに関するルールを追記。

--- StdCell/Hibikino/Glade/hibikino-drc.py
+++ StdCell/Hibikino/Glade/hibikino-drc.py
@@ -1,5 +1,6 @@
 # 2um Hibikino DRC
 # ver1.00 : 2017/1/24: akita11 akita@ifdl.jp
+# ver1.10 : 2017/7/15: akita11 : VIA-Poly/CNA overlap, NWL-PWL/NSL-PSL overlap
 
 # simpe function to print # errors - unused.
 def printErrors(msg) :
@@ -55,8 +56,14 @@ geomSpace(POL, 2, "POL space < 2")
 geomSpace(NWL, 4, "NWL space < 4")
 geomSpace(PWL, 4, "PWL space < 4")
 geomSpace(CST, 4, "CST space < 4")
-geomSpace(VIA, POL, 1, "VIA/POL < 1")
-geomSpace(VIA, CNA, 1, "VIA/CND&CDP < 1")
+
+geomSpace(NSL, 2, "NSL space < 2") # add 170715 by akita11
+geomSpace(PSL, 2, "PSL space < 2") # add 170715 by akita11
+geomSpace(VTN, 2, "VTN space < 2") # add 170715 by akita11
+geomSpace(VTP, 2, "VTP space < 2") # add 170715 by akita11
+
+#geomSpace(VIA, POL, 0, "VIA/POL < 1")
+#geomSpace(VIA, CNA, 1, "VIA/CND&CDP < 1")
 
 print "Check Width"
 geomWidth(ACT, 4, "ACT width < 4")
@@ -95,14 +102,27 @@ print "Check MOS gate extension"
 geomExtension(POL, ACT, 2, "POL gate extension < 2")
 
 print "Check Overlap"
-geomOverlap(NWL, PWL, 0, "NWL/PWL overlap")
-geomOverlap(NSL, PSL, 0, "NSL/PSL overlap")
+#geomOverlap(NWL, PWL, 0, "NWL/PWL overlap")
+NWLPWL = geomAnd(NWL, PWL)
+geomArea(NWLPWL, 0, 0, "NWL-PWL overlap")
+
+#geomOverlap(NSL, PSL, 0, "NSL/PSL overlap")
+NSLPSL = geomAnd(NSL, PSL)
+geomArea(NSLPSL, 0, 0, "NSL-PSL overlap")
+
+#geomSpace(VIA, POL, 0, "VIA/POL < 1")
+POLVIA = geomAnd(POL, VIA)
+geomArea(POLVIA, 0, 0, "Poly-VIA overlap")
+
+#geomSpace(VIA, CNA, 1, "VIA/CND&CDP < 1")
+CNAVIA = geomAnd(VIA, CNA)
+geomArea(CNAVIA, 0, 0, "CNA-VIA overlap")
 
 print "Check stand-alone Active"
 ASL = geomOr(NSL, PSL) # both select
 NASL = geomNot(ASL)
 AACT = geomAnd(ACT, NASL)
-geomArea(AACT, 0, 0, "ACT without NSL/ASL")
+geomArea(AACT, 0, 0, "ACT without NSL/PSL")
 
 # Exit DRC package, freeing memory
 geomEnd()
